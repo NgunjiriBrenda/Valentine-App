58f4a22229267824394376c50595906a659dabfd
brenda njoki <ngunjiribrenda5@gmail.com>
2026-02-02 21:25:01 +0300

    Updates the index.js to answer the Why question

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const yesBtn=document.getElementById('yesBtn');
 const result=document.getElementById('result');
 
 
-noBtn.addEventListener("mouseover", () => {
-    const x = Math.random()* 200-100;
-    const y = Math.random() * 200-100;
-
-    noBtn.style.transform = `translate(${x}px, ${y}px)`;
+noBtn.addEventListener("click", () => {
+    window.location.href= "why.html"; 
+   
 });
+
+yesBtn.addEventListener("click", () => {
+    result.classList.remove("hidden");
+});
+
+if(localStorage.getItem("cameFromWhy") === "true") {
+    result.classList.remove("hidden");
+    localStorage.removeItem("cameFromWhy");
+}
