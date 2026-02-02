18fd1bf280124dd3bc407b0c401e46f0a4651684
brenda njoki <ngunjiribrenda5@gmail.com>
2026-02-02 22:49:13 +0300

    adds confetti functionality

--- index.js
+++ index.js
@@ -16,3 +16,16 @@ if(localStorage.getItem("cameFromWhy") === "true") {
     result.classList.remove("hidden");
     localStorage.removeItem("cameFromWhy");
 }
+
+
+function launchConfetti() {
+    for(let i = 0; i < 30; i++) {
+        const confetti = document.createElement("div");
+        confetti.className = "confetti";
+        confetti.style.left = Math.random() * 100 + "vw";
+        confetti.style.animationDuration = Math.random() * 2 + 2 + "s";
+        document.body.appendChild(confetti);
+
+        setTimeout(() => confetti.remove(), 4000);
+    }
+}
